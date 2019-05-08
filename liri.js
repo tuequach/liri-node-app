52e007f2c7111c7e380b7e61518af1f9334d9146
tuequach <tuequach@gmail.com>
2019-05-08 14:32:11 -0500

    added all required function in switch statements

--- liri.js
+++ liri.js
@@ -11,7 +11,22 @@ var inputPara = process.argv[3];
 
 userInputs (userOpt, inputPara);
 
+//functions in switch statements
+function userInputs (userOpt, inputPara) {
+    switch (userOpt) {
+        case 'concert-this':
+            showConcertInfo(inputPara);
+            break;
+        case 'spotify-this-song':
+            showSongInfo(inputPara);
+            break;
+        case 'movie-this':
+            showMovieInfo(inputPara);
+            break;
+        case 'do-what-it-says':
+            showSomeInfo();
+            break;
+        default:
+            console.log('Invalid!! This is not an Option. Please choose any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says')
+    }
 }
-
-
-
