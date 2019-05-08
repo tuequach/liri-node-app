fe1b5979e5d3784920966738e21c6e7d49a3a3ee
tuequach <tuequach@gmail.com>
2019-05-08 14:14:08 -0500

    added variables and dependencies

--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var fs = require('fs');
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var Spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+var userOpt = process.argv[2];
+var inputPara = process.argv[3];
+
+userInputs (userOpt, inputPara);
+
+}
+
+
+
